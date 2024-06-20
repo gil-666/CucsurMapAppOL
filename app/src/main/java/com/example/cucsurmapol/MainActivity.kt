35e11e5ff2bbdf7ae177c63b29957c1d93966647
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-06-20 16:41:50 -0600

    initial 2

---
+++ app/src/main/java/com/example/cucsurmapol/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.cucsurmapol
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cucsurmapol.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        val mapWebView: WebView = findViewById(R.id.mapWebView)
+        val webSettings: WebSettings = mapWebView.settings
+        webSettings.javaScriptEnabled = true
+        WebView.setWebContentsDebuggingEnabled(true)
+        mapWebView.addJavascriptInterface(WebAppInterface(this),"Android")
+        mapWebView.loadUrl("file:///android_asset/map.html")
+    }
+}
---
+++ app/src/main/java/com/example/cucsurmapol/WebAppInterface.kt
@@ -0,0 +1,13 @@
+package com.example.cucsurmapol
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+
+class WebAppInterface(private val context: Context) {
+    @JavascriptInterface
+    fun sendBuildingInfo(info: String) {
+        // Handle the received building info here
+        Toast.makeText(context, "Building info: $info", Toast.LENGTH_SHORT).show()
+    }
+}
